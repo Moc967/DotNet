f1a5f0309ba3b761ac3fd449bed317dcdcd67935
Magno Carneiro <magnovaldo.carneiro@gmail.com>
2022-01-05 20:44:53 -0300

    Program structure lesson

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 10;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso .NET {i}");
+            }
+        }
+    }
+}
