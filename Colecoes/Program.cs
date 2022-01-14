4239360613f4e310bc8f082b046f79b0bf59f837
Magno Carneiro <magnovaldo.carneiro@gmail.com>
2022-01-14 17:10:23 -0300

    Arrays lesson

---
+++ Colecoes/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayInteiros = new int[3];
+            arrayInteiros[0] = 1;
+            arrayInteiros[1] = 2;
+            arrayInteiros[2] = 3;
+
+            foreach (int i in arrayInteiros)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
